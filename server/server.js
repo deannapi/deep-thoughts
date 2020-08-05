7e3dcc9750c2790bcf34852d1df7e1b5b1551881
deannapi <deannapi.mart@gmail.com>
2020-08-05 10:59:56 -0500

    navigation setup

--- server/server.js
+++ server/server.js
@@ -1,18 +1,20 @@
 const express = require('express');
-const { authMiddleware } = require('./utils/auth');
+const path = require('path');
+const { authMiddleware } = require('./server/utils/auth');
 // import ApolloServer
 const { ApolloServer } = require('apollo-server-express');
 
 // import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./server/schemas');
+const db = require('./server/config/connection');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
 // create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  context: authMiddleware
 });
 
 // integrate our Apollo server with the Express application as middleware
@@ -20,6 +22,15 @@ server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Serve up static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
   app.listen(PORT, () => {
